93cc085d2434debc677b728252af71323b3f9db5
ThomasVrd11 <Thomasviard@orange.fr>
2024-05-20 22:38:26 +0200

    chore: Refactor displayMessage function in console.js file

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,3 +1,7 @@
-module.exports = function displayMessage(chaussure) {
-  console.log(chaussure);
-};
+/* eslint-disable no-console */
+// console.log('Hello World');
+function displayMessage() {
+    console.log('Hello World');
+}
+// displayMessage();
+module.exports = displayMessage;
